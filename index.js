ece75c62299a6ec1e5e2cf14e2ecb0ec5f88bf5c
Dinuka <123373529+DINUKA-01@users.noreply.github.com>
2025-02-24 04:37:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 //Modify your URL here
 var hostURL="https://terrific-contentment-production.up.railway.app";
 //TOGGLE for Shorters
-var use1pt=false;
+var use1pt=true;
 
 
 
