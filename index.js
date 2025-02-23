549962aacc5f1d5099bbff7e2bd623c3a5d991c4
Dinuka <123373529+DINUKA-01@users.noreply.github.com>
2025-02-24 04:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://";
+var hostURL="https://terrific-contentment-production.up.railway.app";
 //TOGGLE for Shorters
 var use1pt=false;
 
