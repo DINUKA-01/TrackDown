f24cca3ad87a592653939b2513253be33f06158e
Dinuka <123373529+DINUKA-01@users.noreply.github.com>
2025-02-24 04:57:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://trackdown-production-99e1.up.railway.app/w/3dhjo39/aHR0cHM6Ly9ucmVlci5jb20=";
+var hostURL="https://trackdown-production-99e1.up.railway.app";
 //TOGGLE for Shorters
 var use1pt=false;
 
