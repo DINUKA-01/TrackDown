816ed8aeb719e3af778df6e6380d3d63156c1200
Dinuka <123373529+DINUKA-01@users.noreply.github.com>
2025-02-23 23:21:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://rtyuj.com/";
+var hostURL="https://xztpdk6m.up.railway.app/";
 //TOGGLE for Shorters
 var use1pt=false;
 
