6752cbfaf96b11c14f8d36af500a3d47c5a81225
Dinuka <123373529+DINUKA-01@users.noreply.github.com>
2025-02-23 22:33:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://cleanuri.com/";
+var hostURL="https://cleanuri.com";
 //TOGGLE for Shorters
 var use1pt=false;
 
